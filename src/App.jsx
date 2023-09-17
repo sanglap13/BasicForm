e1cdf88b3ec96c05d4e874b64c4115fd05fa09a1
sanglap13 <sanglapmridha@gmail.com>
2023-09-17 22:13:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+import { Home } from "./components";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Form from "../shared/Form";
+
+const Home = () => {
+  const initialValues = { username: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    return errors;
+  };
+  {
+    Object.keys(formErrors).length === 0 && isSubmit ? (
+      <div className="ui message success">Signed in successfully</div>
+    ) : (
+      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+    );
+  }
+  return (
+    <div className="home">
+      <Form
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        formErrors={formErrors}
+        formValues={formValues}
+      />
+    </div>
+  );
+};
+
+export default Home;
